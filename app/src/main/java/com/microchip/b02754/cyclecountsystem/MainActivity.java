ec43ae00a824085af05b7c747deef0b408c4a5a2
Keatichai <Keatichai.sucharit@microchip.com>
2016-10-06 11:09:30 +0700

    Intent to SignUp Finish

--- app/src/main/java/com/microchip/b02754/cyclecountsystem/MainActivity.java
+++ app/src/main/java/com/microchip/b02754/cyclecountsystem/MainActivity.java
@@ -1,13 +1,40 @@
 package com.microchip.b02754.cyclecountsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private Button signInButton ,signUpButton ,synButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+        synButton = (Button) findViewById(R.id.button3);
+
+        //Sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }// onclick
+        });
+
+
+
+    } // Main Method
+
+
+
+
+} //Main
